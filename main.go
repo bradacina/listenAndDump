ea7703c1839176f3c8599a7cc8c53357d57efd1c
Ubuntu <animosity@dropchat.chatdrop.q8.internal.cloudapp.net>
2016-09-29 06:19:16 +0000

    correctly proxying requests now

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"bufio"
 	"log"
 	"net/http"
@@ -9,19 +10,62 @@ import (
 	"strings"
 )
 
+func singleJoiningSlash(a, b string) string {
+	if len(b) == 0 || b == "/" {
+		return a
+	}
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+    return a + b
+}
+
+
+func NewProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		dump, _ := httputil.DumpRequest(req, true)
+		log.Printf("%v",string(dump))
+		req.Host = target.Host
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+
+		dump, _ = httputil.DumpRequestOut(req, true)
+		log.Printf("%v",string(dump))
+	}
+
+	return &httputil.ReverseProxy{Director: director}
+}
+
 func main() {
-	url, err := url.Parse("https://knockknock.readify.net/RedPill.svc")
+	f, err := os.Create("listenAndDump.log")
+	defer f.Close()
+	log.SetOutput(f)
+
+	url, err := url.Parse("http://knockknock.readify.net/RedPill.svc")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	proxy := NewProxy(url)
 
 	http.Handle("/", proxy)
 	//http.HandleFunc("/", dump)
 
 	for {
-		err := http.ListenAndServe(":8080", nil)
+		err := http.ListenAndServe(":80", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
